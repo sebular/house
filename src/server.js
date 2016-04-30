c374809ca8dde4ff77ffa9657848352283173812
Sebastian Wood <sebastian.k.wood@gmail.com>
2016-04-30 14:19:23 -0700

    push from mac to work on ubuntu

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
-const express = require('express')
+const config          = require('./config')
+const express         = require('express')
+//const proxy         = require('express-http-proxy')
+const httpProxy       = require('http-proxy')
 
 var app = express()
+var proxy = httpProxy.createProxyServer({
+  autoRewrite: true
+})
 
 app.get('/', (req, res) => {
   res.send("What up house.")
 })
 
-app.listen(1234, function () {
-  console.log('Started on 3000');
+app.get('/downloads', (req,res) => {
+  proxy.web(req, res, { target: 'http://localhost:8080/sabnzbd' })
+})
+
+app.listen(config.port, function () {
+  console.log('Started on ' + config.port);
 });
