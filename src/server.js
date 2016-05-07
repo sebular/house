e2190b9d56eb4206b2a3c1eb43f70e21156d2397
Sebastian <sebastian.k.wood@gmail.com>
2016-05-06 22:54:48 -0700

    static downloads folder, index.html, maybe better proxying to sab

--- src/server.js
+++ src/server.js
@@ -1,21 +1,42 @@
+const path            = require('path')
 const config          = require('./config')
+const bodyParser      = require('body-parser')
 const express         = require('express')
-//const proxy         = require('express-http-proxy')
 const httpProxy       = require('http-proxy')
+const vidStreamer     = require('vid-streamer')
 
-var app = express()
+const app = express()
+
 var proxy = httpProxy.createProxyServer({
-  autoRewrite: true
+  xfwd: true,
 })
 
-app.get('/', (req, res) => {
-  res.send("What up house.")
+const streamerOptions = {
+  "mode": "development",
+  "forceDownload": false,
+  "random": false,
+  "rootFolder": "/home/sebastian/house/src/downloads/",
+  "rootPath": "",
+  "server": "VidStreamer.js/0.1.4"
+}
+
+app.use('/sabnzbd/', (req, res) => {
+  proxy.web(req, res, { target: 'http://localhost:8080/' })
 })
 
-app.get('/downloads', (req,res) => {
-  proxy.web(req, res, { target: 'http://localhost:8080/sabnzbd' })
-})
+//app.use('/downloads/', vidStreamer.settings(streamerOptions))
 
-app.listen(config.port, function () {
+//require('node-jsx').install()
+
+// Include static assets. Not advised for production
+//app.use(express.static(path.join(__dirname, 'public')));
+// Set view path
+//app.set('views', path.join(__dirname, 'views'));
+
+app.use('/downloads', express.static(__dirname + '/downloads'))
+
+app.use('/', express.static(__dirname + '/public'))
+
+app.listen(config.port, '127.0.0.1', function () {
   console.log('Started on ' + config.port);
 });
